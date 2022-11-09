bb9cffaae192a2a8e5be80d860b765b146e88283
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2022-11-09 14:05:37 -0500

    Updates to api integration (#3)

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -1,9 +1,9 @@
 {% macro create_aws_cosmos_api() %}
     {% if target.name == "prod" %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_cosmos_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::661245089684:role/snowflake-api-cosmos' api_allowed_prefixes = (
-            'https://e03pt6v501.execute-api.us-east-1.amazonaws.com/prod/',
-            'https://mryeusnrob.execute-api.us-east-1.amazonaws.com/dev/'
+        CREATE api integration IF NOT EXISTS aws_cosmos_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/snowflake-api-cosmos' api_allowed_prefixes = (
+            'https://z97ik1b2d0.execute-api.us-east-1.amazonaws.com/dev/',
+            'https://dazi3rled6.execute-api.us-east-1.amazonaws.com/prod/'
         ) enabled = TRUE;
 {% endset %}
         {% do run_query(sql) %}
