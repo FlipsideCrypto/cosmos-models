d8c0abc6989c5bfcaa697063d6b75ab32640f1bc
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2022-11-21 16:24:57 -0500

    Add Cosmos Transactions

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -10,6 +10,7 @@
         {% set sql %}
         {# {{ create_udf_get_cosmos_chainhead() }} #}
         {{ create_udf_get_cosmos_blocks() }}
+        {{ create_udf_get_cosmos_transactions() }}
         {% endset %}
         {% do run_query(sql) %}
 {% endmacro %}
--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -9,6 +9,17 @@
     {%- endif %};
 {% endmacro %}
 
+{% macro create_udf_get_cosmos_transactions() %}
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_get_cosmos_transactions(
+        json variant
+    ) returns text api_integration = aws_cosmos_api AS {% if target.name == "prod" %}
+        'https://dazi3rled6.execute-api.us-east-1.amazonaws.com/prod/bulk_get_cosmos_transactions'
+    {% else %}
+        'https://z97ik1b2d0.execute-api.us-east-1.amazonaws.com/dev/bulk_get_cosmos_transactions'
+    {%- endif %};
+{% endmacro %}
+
 {# {% macro create_udf_get_cosmos_chainhead() %}
     CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_cosmos_api AS {% if target.name == "prod" %}
         'https://e03pt6v501.execute-api.us-east-1.amazonaws.com/prod/get_cosmos_chainhead'
