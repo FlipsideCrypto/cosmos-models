775fa2ea9132378ee05202e48f8c80cb3a9161d7
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2022-12-08 10:50:39 -0500

    Pagination backfill

---
+++ models/streamline/streamline__transactions_pagination.sql
@@ -0,0 +1,17 @@
+{{ config (
+    materialized = "view",
+    post_hook = if_data_call_function(
+        func = "{{this.schema}}.udf_get_cosmos_transactions(object_construct('sql_source', '{{this.identifier}}'))",
+        target = "{{this.schema}}.{{this.identifier}}"
+    )
+) }}
+
+SELECT
+    block_number
+FROM
+    {{ source(
+        "bronze_streamline",
+        "tx_search"
+    ) }}
+WHERE
+    DATA:result:total_count :: INT > 100
