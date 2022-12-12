f67e9d691fcf08fa2c9f8b01e6e9af2324244132
jhuhnke <jessica@flipsidecrypto.com>
2022-12-12 06:15:48 -0600

    validator token and labels

---
+++ models/gold/core__fact_validators.sql
@@ -0,0 +1,23 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT
+    address,
+    blockchain,
+    creator,
+    label_type,
+    label_subtype,
+    label,
+    project_name,
+    account_address,
+    delegator_shares,
+    self_delegation,
+    num_delegators,
+    rate,
+    RANK,
+    num_governance_votes,
+    raw_metadata,
+    _unique_key
+FROM
+    {{ ref('silver__validators') }}
---
+++ models/silver/silver__validators.sql
@@ -0,0 +1,32 @@
+{{ config(
+    materialized = 'table'
+) }}
+
+SELECT
+    address,
+    'cosmos' AS blockchain,
+    'flipside' AS creator,
+    'operator' AS label_type,
+    'validator' AS label_subtype,
+    DATA :title :: STRING AS label,
+    DATA :cons_address :: STRING AS project_name,
+    DATA :acc_address :: STRING AS account_address,
+    DATA :power :: NUMBER AS delegator_shares,
+    DATA :self_stake :: NUMBER AS self_delegation,
+    DATA :delegators :: NUMBER AS num_delegators,
+    DATA :fee :: NUMBER AS rate,
+    RANK() over (
+        PARTITION BY address
+        ORDER BY
+            DATA :self_stake DESC
+    ) AS RANK,
+    DATA :governance_votes :: NUMBER AS num_governance_votes,
+    DATA AS raw_metadata,
+    concat_ws(
+        '-',
+        address,
+        creator,
+        blockchain
+    ) AS _unique_key
+FROM
+    {{ ref('bronze_api__get_validator_metadata') }}
