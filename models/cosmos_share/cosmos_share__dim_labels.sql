2c4de94f40ddec793d187afce6a775d22ae2f84f
jhuhnke <jessica@flipsidecrypto.com>
2022-12-16 06:02:17 -0600

    incremental fix

--- models/cosmos_share/cosmos_share__dim_labels.sql
+++ models/cosmos_share/cosmos_share__dim_labels.sql
@@ -1,6 +1,6 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = "CONCAT_WS('-', blockchain, address, creator)",
+    unique_key = "unique_key",
     tags = ['share']
 ) }}
 
@@ -11,6 +11,12 @@ SELECT
     label_type,
     label_subtype,
     label,
-    project_name
+    project_name, 
+    concat_ws(
+        '-',
+        blockchain,
+        address,
+        creator
+    ) AS unique_key
 FROM
     {{ ref('core__dim_labels') }}
