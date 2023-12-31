ff2679767c2a6d02b80ee80aa364bd6bf17a7d73
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-12-30 22:46:12 -0500

    hub 1-3 into gold blocks and transactions

--- models/gold/core/core__fact_blocks.sql
+++ models/gold/core/core__fact_blocks.sql
@@ -26,3 +26,87 @@ SELECT
     ) AS modified_timestamp
 FROM
     {{ ref('silver__blocks') }}
+UNION ALL
+SELECT
+    block_id,
+    block_timestamp,
+    blockchain,
+    chain_id,
+    COALESCE(
+        tx_count,
+        0
+    ),
+    proposer_address,
+    validator_hash,
+    COALESCE (
+        blocks_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['block_id']
+        ) }}
+    ) AS fact_blocks_id,
+    COALESCE(
+        inserted_timestamp,
+        '2000-01-01'
+    ) AS inserted_timestamp,
+    COALESCE(
+        modified_timestamp,
+        '2000-01-01'
+    ) AS modified_timestamp
+FROM
+    {{ ref('silver__blocks_ch1') }}
+UNION ALL
+SELECT
+    block_id,
+    block_timestamp,
+    blockchain,
+    chain_id,
+    COALESCE(
+        tx_count,
+        0
+    ),
+    proposer_address,
+    validator_hash,
+    COALESCE (
+        blocks_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['block_id']
+        ) }}
+    ) AS fact_blocks_id,
+    COALESCE(
+        inserted_timestamp,
+        '2000-01-01'
+    ) AS inserted_timestamp,
+    COALESCE(
+        modified_timestamp,
+        '2000-01-01'
+    ) AS modified_timestamp
+FROM
+    {{ ref('silver__blocks_ch2') }}
+UNION ALL
+SELECT
+    block_id,
+    block_timestamp,
+    blockchain,
+    chain_id,
+    COALESCE(
+        tx_count,
+        0
+    ),
+    proposer_address,
+    validator_hash,
+    COALESCE (
+        blocks_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['block_id']
+        ) }}
+    ) AS fact_blocks_id,
+    COALESCE(
+        inserted_timestamp,
+        '2000-01-01'
+    ) AS inserted_timestamp,
+    COALESCE(
+        modified_timestamp,
+        '2000-01-01'
+    ) AS modified_timestamp
+FROM
+    {{ ref('silver__blocks_ch3') }}
--- models/gold/core/core__fact_transactions.sql
+++ models/gold/core/core__fact_transactions.sql
@@ -34,3 +34,102 @@ SELECT
   ) AS modified_timestamp
 FROM
   {{ ref('silver__transactions_final') }}
+UNION ALL
+SELECT
+  block_id,
+  block_timestamp,
+  tx_id,
+  NULL AS tx_from,
+  tx_succeeded,
+  codespace,
+  NULL AS fee,
+  NULL AS fee_denom,
+  gas_used,
+  gas_wanted,
+  tx_code,
+  tx_log,
+  msgs,
+  _inserted_timestamp,
+  unique_key,
+  COALESCE (
+    transactions_id,
+    {{ dbt_utils.generate_surrogate_key(
+      ['tx_id']
+    ) }}
+  ) AS fact_transactions_id,
+  COALESCE(
+    inserted_timestamp,
+    '2000-01-01'
+  ) AS inserted_timestamp,
+  COALESCE(
+    modified_timestamp,
+    '2000-01-01'
+  ) AS modified_timestamp
+FROM
+  {{ ref('silver__transactions_ch1') }}
+UNION ALL
+SELECT
+  block_id,
+  block_timestamp,
+  tx_id,
+  NULL AS tx_from,
+  tx_succeeded,
+  codespace,
+  NULL AS fee,
+  NULL AS fee_denom,
+  gas_used,
+  gas_wanted,
+  tx_code,
+  tx_log,
+  msgs,
+  _inserted_timestamp,
+  unique_key,
+  COALESCE (
+    transactions_id,
+    {{ dbt_utils.generate_surrogate_key(
+      ['tx_id']
+    ) }}
+  ) AS fact_transactions_id,
+  COALESCE(
+    inserted_timestamp,
+    '2000-01-01'
+  ) AS inserted_timestamp,
+  COALESCE(
+    modified_timestamp,
+    '2000-01-01'
+  ) AS modified_timestamp
+FROM
+  {{ ref('silver__transactions_ch2') }}
+UNION ALL
+SELECT
+  block_id,
+  block_timestamp,
+  tx_id,
+  NULL AS tx_from,
+  tx_succeeded,
+  codespace,
+  NULL AS fee,
+  NULL AS fee_denom,
+  gas_used,
+  gas_wanted,
+  tx_code,
+  tx_log,
+  msgs,
+  _inserted_timestamp,
+  unique_key,
+  COALESCE (
+    transactions_id,
+    {{ dbt_utils.generate_surrogate_key(
+      ['tx_id']
+    ) }}
+  ) AS fact_transactions_id,
+  COALESCE(
+    inserted_timestamp,
+    '2000-01-01'
+  ) AS inserted_timestamp,
+  COALESCE(
+    modified_timestamp,
+    '2000-01-01'
+  ) AS modified_timestamp
+FROM
+  {{ ref('silver__transactions_ch3') }}
