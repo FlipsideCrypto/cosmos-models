93f2d638eec2b1781eaecbe27a0197e075cddd25
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-05-29 10:07:52 -0400

    prod endpoints

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -1,8 +1,8 @@
 {% macro create_aws_cosmos_api() %}
     {% if target.name == "prod" %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_cosmos_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/cosmos-api-prod-rolesnowflakeudfsAF733095-14KYNLUQ3CWV2' api_allowed_prefixes = (
-            'https://bp6s0ib6fk.execute-api.us-east-1.amazonaws.com/prod/'
+        CREATE api integration IF NOT EXISTS aws_cosmos_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::924682671219:role/cosmos-api-prod-rolesnowflakeudfsAF733095-p9k0jZbJcQpS' api_allowed_prefixes = (
+            'https://kpg3w2qkm4.execute-api.us-east-1.amazonaws.com/prod/'
         ) enabled = TRUE;
 {% endset %}
         {% do run_query(sql) %}
--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -3,7 +3,7 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_rest_api_v2(
         json OBJECT
     ) returns ARRAY api_integration = {% if target.name == "prod" %}
-        aws_cosmos_api_prod AS ''
+        aws_cosmos_api_prod AS 'https://kpg3w2qkm4.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_rest_api'
     {% else %}
         aws_cosmos_api_stg AS 'https://e8nbzsw4r9.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api'
     {%- endif %};
