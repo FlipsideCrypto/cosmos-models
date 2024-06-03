5cc391b1c1155b86b442bc8a6cc869dca5be0328
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-06-03 16:17:53 -0400

    5k sql limit

--- models/streamline/silver/realtime/streamline__blocks_realtime.sql
+++ models/streamline/silver/realtime/streamline__blocks_realtime.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"blocks_v2",
-        "sql_limit" :"1000",
+        "sql_limit" :"5000",
         "producer_batch_size" :"20",
         "worker_batch_size" :"10",
         "sql_source" :"{{this.identifier}}" }
