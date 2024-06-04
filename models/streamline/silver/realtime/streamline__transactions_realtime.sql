1ea99e34e2e0cc5c091dccdf61bcc96d34aac6ec
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-06-03 20:21:15 -0400

    800 test

--- models/streamline/silver/realtime/streamline__transactions_realtime.sql
+++ models/streamline/silver/realtime/streamline__transactions_realtime.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"transactions_v2",
-        "sql_limit" :"5000",
+        "sql_limit" :"800",
         "producer_batch_size" :"40",
         "worker_batch_size" :"20",
         "exploded_key": "[\"result.txs\"]",
